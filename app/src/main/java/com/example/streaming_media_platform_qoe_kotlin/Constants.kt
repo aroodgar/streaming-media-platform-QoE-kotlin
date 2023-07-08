3c2f7e02902a0ed1e2e1f056db98e0d761298ed2
aroodgar <2000amir1379@gmail.com>
2023-07-08 19:28:00 +0330

    fix: reverted url constant

--- app/src/main/java/com/example/streaming_media_platform_qoe_kotlin/Constants.kt
+++ app/src/main/java/com/example/streaming_media_platform_qoe_kotlin/Constants.kt
@@ -2,8 +2,7 @@ package com.example.streaming_media_platform_qoe_kotlin
 
 object Constants {
 //    val RTL_STREAM_URL = "http://icecast.rtl.fr/rtl-1-44-128?listen=webBAIOCwsGDQQBAgsBBAUHAQ"
-//    val RTL_STREAM_URL = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1"
-    val RTL_STREAM_URL = "https://video.varzesh3.com/video/304882/%D9%87%D9%88%D8%A7%D8%AF%D8%A7%D8%B1-%D8%AC%D8%A7%D9%84%D8%A8-%D8%A7%D8%B3%D8%AA%D9%82%D9%84%D8%A7%D9%84-%D9%85%D9%82%D8%A7%D8%A8%D9%84-%D8%B3%D8%A7%D8%AE%D8%AA%D9%85%D8%A7%D9%86-%D8%A8%D8%A7%D8%B4%DA%AF%D8%A7%D9%87?utm_source=video-widget"
+    val RTL_STREAM_URL = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1"
     val STREAM_URL_KEY = "stream url"
     val CONNECT_TIMEOUT_KEY = "connect timeout"
     val READ_TIMEOUT_KEY = "read timeout"
